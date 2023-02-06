71ffaecfcda5b71ad49790ee4db6f6f5987cf37f
Fezard Léo <leo.fezard33@gmail.com>
2023-02-06 23:29:37 +0100

    [UPDATE] auth + private route

--- Desktop/personal-blog/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { UserContextProvider } from './Contexts/UserContext'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <UserContextProvider>
-        <App />
-    </UserContextProvider>
-)
