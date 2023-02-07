6c9f17f6152c1a0be97a344885be0a8cfb65def6
Fezard Léo <leo.fezard33@gmail.com>
2023-02-07 14:46:54 +0100

    [UPDATE] auth with environment variables

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -3,14 +3,27 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_APIKEY,
-//   authDomain: process.env.FIREBASE_AUTHDOMAIN,
-//   projectId: process.env.FIREBASE_PROJET_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
+const firebaseConfig = {
+  apiKey: JSON.stringify(import.meta.env.VITE_REACT_FIREBASE_APIKEY).replace(
+    /['"]+/g,
+    ""
+  ),
+  authDomain: JSON.stringify(
+    import.meta.env.VITE_REACT_REACT_FIREBASE_AUTHDOMAIN
+  ).replace(/['"]+/g, ""),
+  projectId: JSON.stringify(
+    import.meta.env.VITE_REACT_REACT_FIREBASE_PROJET_ID
+  ).replace(/['"]+/g, ""),
+  storageBucket: JSON.stringify(
+    import.meta.env.VITE_REACT_REACT_FIREBASE_STORAGE_BUCKET
+  ).replace(/['"]+/g, ""),
+  messagingSenderId: JSON.stringify(
+    import.meta.env.VITE_REACT_REACT_FIREBASE_MESSAGING_SENDER_ID
+  ).replace(/['"]+/g, ""),
+  appId: JSON.stringify(
+    import.meta.env.VITE_REACT_REACT_FIREBASE_APP_ID
+  ).replace(/['"]+/g, ""),
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
